a2c62b1fb8480f4a7ef220cc01145139e471fd99
hwnagsae91 <hhyong91@gmail.com>
2022-09-17 12:26:15 +0900

    exploration ch2. 2/2

--- exploration/220913/220913_2.py
+++ exploration/220913/220913_2.py
@@ -16,6 +16,7 @@ x축에 X 데이터 중 humidity 데이터를, y축에는 count 데이터를 넣
 
 import os
 import pandas as pd
+import numpy as np
 from pathlib import Path
 import seaborn as sns
 from matplotlib import pyplot as plt
@@ -30,6 +31,7 @@ tarins_kwargs = {"random_state":17,"test_size":0.2}
 
 # 1.
 df = pd.read_csv(os.path.join(BASE_DIR,"data/bike.csv"))
+
 # 2.
 df["datetime"] = pd.to_datetime(df["datetime"])
 df["year"] = df["datetime"].dt.year
@@ -57,23 +59,34 @@ plt.show()
 df.drop(columns=useless_cols, inplace=True)
 X_cols = list(df.columns)
 X_cols.remove(y_col)
-y = df.loc[:,[y_col]].to_numpy()
-X = df.loc[:,X_cols].to_numpy()
+y = df[y_col]
+X = df.loc[:,X_cols]
 
 # 5.
-X_train, X_test, y_train, y_test = train_test_split(X,y,**tarins_kwargs)
+X_train, X_test, y_train, y_test = train_test_split(X, y, **tarins_kwargs)
 
 model = LinearRegression()
-model.fit(X_train, y_train)
+model.fit(X_train.to_numpy(), y_train.to_numpy())
 
 # 6.
-pred_y = model.predict(X_test)
+pred_y = model.predict(X_test.to_numpy())
 mse = ((pred_y - y_test)**2).mean()
 rmse = mse**0.5
 print(f" mse: {mse}\n rmse: {rmse}")
 
+# 후처리
+# 0.5명은 사람이 아니므로 0으로 판단
+# 같은 맥락으로 인원의 수가 음수일 경우도 없으므로 음수의 값음 0으로 처리
+# ※ MSE, RMSE값은 모델의 성능을 평가하는 것이므로 예측된 값 자체를 평가지표로 해야한다고 판단
+pred_y =  pred_y.astype(np.int64)
+pred_y[pred_y < 0] = 0
+
 # 7.
-plt.scatter(X_test[:, 0], y_test, label="true")
-plt.scatter(X_test[:, 0], pred_y, label="pred")
-plt.legend()
+fig, (ax1, ax2) = plt.subplots(2,1)
+ax1.scatter(X_test["temp"], pred_y, label="pred")
+ax1.scatter(X_test["temp"], y_test, label="true")
+ax1.legend()
+ax2.scatter(X_test["humidity"], pred_y, label="pred")
+ax2.scatter(X_test["humidity"], y_test, label="pred")
+ax2.legend()
 plt.show()
